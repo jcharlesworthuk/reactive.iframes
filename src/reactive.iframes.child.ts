dae699b78dbef0d79393ed359e3c5a7beb7a5964
James Charlesworth <jcharlesworth@citywire.co.uk>
2016-02-23 15:36:15 +0000

    Initial commit

---
+++ src/reactive.iframes.child.ts
@@ -0,0 +1,80 @@
+
+module reactive.iframes {
+
+    const MessagePrefix = 'ReactiveIFrames';
+    const MessageDelimiter = '~';
+    const FrameIdAttributeName = 'data-frameid';
+    const FrameScriptId = 'iframe_child_script';
+
+    export class Child {
+        messageRegex : RegExp
+        id: string
+        parentWidth: number
+
+        constructor() {
+            var scriptTag = document.getElementById(FrameScriptId);
+            if (!scriptTag)
+                throw new Error('The ReactiveIFrames child script requires an id of "' + FrameScriptId  + '" on the <script> tag that included it');
+
+            if (!scriptTag.hasAttribute(FrameIdAttributeName))
+                throw new Error('The ReactiveIFrames child script requires a data attribute called "' + FrameIdAttributeName + '" on the <script> tag that included it');
+
+
+            this.id = scriptTag.getAttribute('data-frameid');
+
+            this.messageRegex = new RegExp('^' + MessagePrefix + MessageDelimiter + this.id + MessageDelimiter + '(\\S+)' + MessageDelimiter + '(.+)$');
+
+            window.addEventListener('message', this.processMessage.bind(this), false);
+            
+            this.parentWidth = null;
+            this.sendMessage('height', this.currentHeight);
+        }
+
+        processMessage(event: Event) {
+            
+            if (!(event instanceof MessageEvent) || typeof <MessageEvent>event.data !== 'string') {
+                return;
+            }
+            var messageData = <string>(<MessageEvent>event).data;
+
+            var match = messageData.match(this.messageRegex);
+
+            if (!match || match.length !== 3) { return; }
+
+            var key = match[1];
+            var value = match[2];
+            if (key === 'width') {
+                var width = parseInt(value);
+                console.log("Parent -> " + width + " W Child (" + this.id + ")");
+                // Change the width if it's different.
+                if (width !== this.parentWidth) {
+                    this.parentWidth = width;
+                    this.sendMessage('height', this.currentHeight);
+                }
+            }
+        }
+
+        sendMessage(messageType: string, message: string) {
+            window.parent.postMessage(this.makeMessage(messageType, message), '*');
+        }
+
+        private makeMessage(messageType: string, message: string) {
+            var bits = [MessagePrefix, this.id, messageType, message];
+            return bits.join(MessageDelimiter);
+        }   
+        
+        get currentHeight() : string {
+            return document.body.offsetHeight.toString();
+        }
+    }
+}
+
+(function () {
+    var oldWindowLoad = window.onload;
+    window.onload = function (e: Event) {
+        new reactive.iframes.Child();
+        if (oldWindowLoad) {
+            oldWindowLoad(e);
+        }
+    };
+})();
---
+++ src/reactive.iframes.parent.ts
@@ -0,0 +1,60 @@
+
+module reactive.iframes {
+    
+    const MessagePrefix = 'ReactiveIFrames';
+    const MessageDelimiter = '~';
+
+    export class Parent {
+
+        url: string
+        config: any
+        messageRegex: RegExp
+
+        constructor(private element: HTMLElement, private id: string) {
+            window.addEventListener('resize', this.onResize);
+            this.messageRegex = new RegExp('^' + MessagePrefix + MessageDelimiter + id + MessageDelimiter + '(\\S+)' + MessageDelimiter + '(.+)$');
+            window.addEventListener('message', this.processMessage.bind(this), false);
+        }
+        
+        private onResize = (event: Event) => {
+            this.sendWidth();
+        }
+
+        sendWidth() {
+            var width = this.element.offsetWidth.toString();
+            this.sendMessage('width', width);
+        }
+
+        sendMessage(messageType: string, message: string) {
+            this.iframe.contentWindow.postMessage(this.makeMessage(messageType, message), '*');
+        }
+
+        private makeMessage(messageType: string, message: string) {
+            var bits = [MessagePrefix, this.id, messageType, message];
+            return bits.join(MessageDelimiter);
+        }
+
+        get iframe() {
+            return this.element.getElementsByTagName('iframe')[0];
+        }
+
+        processMessage(event: Event) {
+            if (!(event instanceof MessageEvent) || typeof <MessageEvent>event.data !== 'string') {
+                return;
+            }
+            var messageData = <string>(<MessageEvent>event).data;
+
+            var match = messageData.match(this.messageRegex);
+            if (!match || match.length !== 3) { return; }
+
+            var key = match[1];
+            var value = match[2];
+            if (key === 'height') {
+                var height = parseInt(value);
+                console.log("Child -> " + height + " H Parent (" + this.id + ")");
+                this.iframe.setAttribute('height', height + 'px');
+            }
+        }
+    }
+
+}
